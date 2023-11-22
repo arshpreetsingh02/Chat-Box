9208094c0e54695fb3fcbc12b809861c0c008c81
Arshpreet Singh <140311925+arshpreetsingh02@users.noreply.github.com>
2023-11-22 21:59:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+const userMessage = [
+    ["hi", "hey", "hello","Hi", "Hello","hlo","Hey"],
+    ["suggest me some stones", "suggest me some gems", "suggest me some jewels","recommand me some stones"],
+    ["how can i get them", "how to purchase","how can i get that"],
+    
+  ];
+  const botReply = [
+    ["Hey!",],
+    ["Ruby & emerald stones"],
+    ["Links are available on website" ,],
+  ]
+  
+  const alternative = [
+    "Request declined",
+  ];
+  
+  const synth = window.speechSynthesis;
+  
+  function voiceControl(string) {
+    let u = new SpeechSynthesisUtterance(string);
+    u.text = string;
+    u.lang = "en-aus";
+    u.volume = 1;
+    u.rate = 1;
+    u.pitch = 1;
+    synth.speak(u);
+  }
+  
+  function sendMessage() {
+    const inputField = document.getElementById("input");
+    let input = inputField.value.trim();
+    input != "" && output(input);
+    inputField.value = "";
+  }
+  document.addEventListener("DOMContentLoaded", () => {
+    const inputField = document.getElementById("input");
+    inputField.addEventListener("keydown", function (e) {
+      if (e.code === "Enter") {
+        let input = inputField.value.trim();
+        input != "" && output(input);
+        inputField.value = "";
+      }
+    });
+  });
+  
+  function output(input) {
+    let product;
+  
+    let text = input.toLowerCase().replace(/[^\w\s\d]/gi, "");
+  
+    text = text
+      .replace(/[\W_]/g, " ")
+      .replace(/ a /g, " ")
+      .replace(/i feel /g, "")
+      .replace(/whats/g, "what is")
+      .replace(/please /g, "")
+      .replace(/ please/g, "")
+      .trim();
+  
+    let comparedText = compare(userMessage, botReply, text);
+  
+    product = comparedText
+      ? comparedText
+      : alternative[Math.floor(Math.random() * alternative.length)];
+    addChat(input, product);
+  }
+  
+  function compare(triggerArray, replyArray, string) {
+    let item;
+    for (let x = 0; x < triggerArray.length; x++) {
+      for (let y = 0; y < replyArray.length; y++) {
+        if (triggerArray[x][y] == string) {
+          items = replyArray[x];
+          item = items[Math.floor(Math.random() * items.length)];
+        }
+      }
+    }
+    //containMessageCheck(string);
+    if (item) return item;
+    else return containMessageCheck(string);
+  }
+  
+  function containMessageCheck(string) {
+    let expectedReply = [
+      [
+        "You'r welcome",
+      ]
+    ];
+    let expectedMessage = [
+      ["ok rhank you", "thanks", "thank you"],
+      
+    ];
+    let item;
+    for (let x = 0; x < expectedMessage.length; x++) {
+      if (expectedMessage[x].includes(string)) {
+        items = expectedReply[x];
+        item = items[Math.floor(Math.random() * items.length)];
+      }
+    }
+    return item;
+  }
+  function addChat(input, product) {
+    const mainDiv = document.getElementById("message-section");
+    let userDiv = document.createElement("div");
+    userDiv.id = "user";
+    userDiv.classList.add("message");
+    userDiv.innerHTML = `<span id="user-response">${input}</span>`;
+    mainDiv.appendChild(userDiv);
+  
+    let botDiv = document.createElement("div");
+    botDiv.id = "bot";
+    botDiv.classList.add("message");
+    botDiv.innerHTML = `<span id="bot-response">${product}</span>`;
+    mainDiv.appendChild(botDiv);
+    var scroll = document.getElementById("message-section");
+    scroll.scrollTop = scroll.scrollHeight;
+    voiceControl(product);
+  }
